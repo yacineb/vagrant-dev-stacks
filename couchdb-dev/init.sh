108a5e1448f2bc30fdbffcbb5531333ad392a10f
yacineb <ybsoft2011@live.com>
2019-03-16 16:27:19 +0100

    add couchdb admin tool

---
+++ couchdb-dev/init.sh
@@ -0,0 +1,7 @@
+#doc https://github.com/jo/couchdb-best-practices#basics
+
+#build docker admin image
+
+#add database
+docker run --net=host --rm couchdb-admin couchdb-ensure http://localhost:5984/test2
+
